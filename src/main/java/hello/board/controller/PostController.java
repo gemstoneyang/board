e7310745e8e01664debbb27a4080fb47f78d52a8
gemstoneyang <bennyws99@gmail.com>
2022-04-21 11:51:56 +0900

    게시물 조회 기능 추가

--- src/main/java/hello/board/controller/PostController.java
+++ src/main/java/hello/board/controller/PostController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Slf4j
@@ -21,4 +22,16 @@ public class PostController {
         model.addAttribute("posts", postRepository.findAll());
         return "main-page";
     }
+
+    @GetMapping("/view/{postId}")
+    public String postView(@PathVariable Long postId, Model model) {
+        log.info("postview");
+        model.addAttribute("post", postRepository.findById(postId));
+        return "post";
+    }
+
+//    @GetMapping("/add")
+//    public String addForm() {
+//        return "addForm";
+//    }
 }
